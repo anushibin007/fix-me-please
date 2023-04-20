3141cfb8f47ce7f2ce6aa9b6ba0f410c7afdc08f
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-04-20 11:44:39 +0530

    Complicate Spring Boot App

--- 2-spring-boot-web/src/main/java/com/jas/controller/SumOfTwoController.java
+++ 2-spring-boot-web/src/main/java/com/jas/controller/SumOfTwoController.java
@@ -1,16 +1,13 @@
 package com.jas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.jas.service.SumService;
 
 public class SumOfTwoController {
 
 	SumService sumService;
 
-	@RequestMapping(value = "/add")
-	public int add(@RequestParam("a") int a, @RequestParam("b") int b) {
+	public int add(int a, int b) {
 		return sumService.add(a, b);
 	}
+
 }
