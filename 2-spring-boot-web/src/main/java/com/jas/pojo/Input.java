ef4dbb6016770544b4e1621f1fee7c894dec4967
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-07-13 14:28:07 +0530

    Increase Spring Question Complexity

---
+++ 2-spring-boot-web/src/main/java/com/jas/pojo/Input.java
@@ -0,0 +1,33 @@
+package com.jas.pojo;
+
+public class Input {
+	private int a;
+	private int b;
+
+	public Input(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "Input [a=" + a + ", b=" + b + "]";
+	}
+
+}
--- 2-spring-boot-web/src/main/java/com/jas/service/SumServiceImpl.java
+++ 2-spring-boot-web/src/main/java/com/jas/service/SumServiceImpl.java
@@ -1,8 +1,23 @@
 package com.jas.service;
 
+import java.util.Map;
+
+import com.jas.pojo.Input;
+
 public class SumServiceImpl implements SumService {
+
+	Map<Input, Integer> cache = null;
+
 	@Override
 	public int add(int a, int b) {
-		return a * b;
+		return add(new Input(a, b));
+	}
+
+	private int add(Input input) {
+		Integer sum = cache.get(input);
+		if (Integer.parseInt(input.toString()) == 0) {
+			return sum;
+		}
+		return input.getA() * input.getB();
 	}
 }
